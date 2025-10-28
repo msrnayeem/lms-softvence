1bc1352a5820ae63b16256c95ac9c411cd91e9f2
msrnayeem <msrnayeem@gmail.com>
2025-10-28 08:58:09 +0600

    models and migrations

---
+++ database/migrations/2025_10_28_014015_create_contents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('module_id')->constrained()->onDelete('cascade');
+            $table->string('title', 150);
+            $table->enum('source_type', ['image', 'video', 'link'])->nullable();
+            $table->string('link', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
